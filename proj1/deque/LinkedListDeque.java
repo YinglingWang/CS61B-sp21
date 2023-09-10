8ac04394fe66bae1cf08dce61cc8e369c6603804
YinglingWang <ylwangserendipity@outlook.com>
2023-09-10 02:20:21 -0700

    Proj1.3 fix

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -2,7 +2,7 @@ package deque;
 
 import java.util.Iterator;
 
-public class LinkedListDeque<T> implements Deque<T> {
+public class LinkedListDeque<T> implements Iterable<T>, Deque<T> {
     private Node sentinel;
     private int size;
 
